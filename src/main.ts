8a2c98dc4ef2acdf44bede762290901a8e7c6303
Carlos Marques <calosmarqu@gmail.com>
2025-07-09 09:47:27 +0100

    update code cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { enviroment } from './enviroment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // ou ['http://localhost:3000'] para limitar
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
+
   await app.listen(enviroment.SERVER_PORT);
 }
 
